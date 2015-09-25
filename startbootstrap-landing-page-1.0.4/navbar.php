011718c2ab00f188d252c2e1e76be6987526eb98
salvador pavon <salvahp@gmail.com>
2015-09-25 14:17:45 +0200

    fix
    fix

--- startbootstrap-landing-page-1.0.4/navbar.php
+++ startbootstrap-landing-page-1.0.4/navbar.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand topnav" href="#">Langt logo</a>
+                <a class="navbar-brand topnav" href="index.php">Langt logo</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
            
