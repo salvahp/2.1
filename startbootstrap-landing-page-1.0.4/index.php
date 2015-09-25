ab4347538fe854c82c6cfec3b3064ee604a35db8
amandavejle <vejle.amanda@gmail.com>
2015-09-25 13:55:18 +0200

    forsøgt dropdown
    virkede ikke

--- startbootstrap-landing-page-1.0.4/index.php
+++ startbootstrap-landing-page-1.0.4/index.php
@@ -125,7 +125,7 @@
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Google Web Fonts and<br>Font Awesome Icons</h2>
+                    <h2 class="section-heading">Bliv medlem<br>det er sjooooovt</h2>
                     <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
@@ -175,14 +175,12 @@
     </div>
     <!-- /.banner -->
 
-<<<<<<< HEAD
 	<?php include "footer.php";?>
     
-=======
+
     <!-- Footer -->
 <?php include "startbootstrap-landing-page-1.0.4/footer.php";?>
 
->>>>>>> origin/master
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
