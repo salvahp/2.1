11ec5b3e9414d5b8239226f108b424368b3af081
salvador pavon <salvahp@gmail.com>
2015-09-30 20:42:47 +0200

    sad
    asd

--- startbootstrap-landing-page-1.0.4/omajk.php
+++ startbootstrap-landing-page-1.0.4/omajk.php
@@ -44,13 +44,11 @@ include "header.php";?>
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-<<<<<<< HEAD
+
                     <h2 class="section-heading">Bestyrrelsen</h2>
                     <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
-=======
-                    <h2 class="section-heading">BESTYRRELSEN</h2>
-                    <p class="lead"> .....</p>
->>>>>>> origin/master
+
+                   
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
                     <img class="img-responsive" src="img/dog.png" alt="">
@@ -71,13 +69,11 @@ include "header.php";?>
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-<<<<<<< HEAD
+
                     <h2 class="section-heading">Kontakt</h2>
                     <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
-=======
-                    <h2 class="section-heading">KONTAKT AJK</h2>
-                    <p class="lead">......</p>
->>>>>>> origin/master
+
+  
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                     <img class="img-responsive" src="img/phones.png" alt="">
