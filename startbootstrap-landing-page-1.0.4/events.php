989029e665f5a4e02da0ce98ff0e4f1305044e5b
amandavejle <vejle.amanda@gmail.com>
2015-09-30 15:04:27 +0200

    band beskrivelser

--- startbootstrap-landing-page-1.0.4/events.php
+++ startbootstrap-landing-page-1.0.4/events.php
@@ -11,8 +11,8 @@ include "header.php";?>
 <!-- Page Header -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Events 
-                    <small>Her kan man se events osv.. </small>
+                <h1 class="page-header"> Events 
+                    <small></small>
                 </h1>
             </div>
         </div>
@@ -27,7 +27,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Martinis </a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+                <p>Den dejlige sangerinde Kira Martini var én af de store overraskelser på programmet i 2014. Nu er hun tilbage med sit stjernespækkede orkester, og vi glæder os til at præsentere dette spændende samarbejde på tværs af generationer for et endnu større publikum. 
+Kira Martini: vokal. Mads Søndergaard: piano. Peter Marott: trompet. Elith ”Nulle” Nykjær: klarinet. Ole ”Fessor” Lindgren: trombone. Joel Illerhag: bas. Morten Ærø: trommer.</p>
             </div>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
@@ -36,7 +37,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Mads Tolling</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 				varius vitae.</p>
+                <p>Den danske violinist Mads Tolling har nu i 15 år haft en stor karriere i USA, men glemmer aldrig, at det var et møde som 15-årig med Svend Asmussen, der for alvor satte ham i gang. I anledning af Asmussens 100- årsdag er Tolling på Danmarksturne med et mix fra Asmussens store repertoire. Med på holdet er selvfølgelig Asmussens guitarist gennem 15 år, Jacob Fischer. 
+Mads Tolling: violin. Jacob Fischer: guitar. Kasper Tagen: kontrabas. Snorre Kirk: trommer.</p>
             </div>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
@@ -45,7 +47,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Paul Harrison</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+                <p>Fuldblodsjazz leveret af et internationalt professionelt team. Denne gang præsenterer han to af sine mangeårige musikalske venner, Sean Moyses og Niklas Carlsson. Der er lagt i ovnen til swingende hot jazz, hvor Harrison som vanligt styrer sine tropper med overbevisende træfsikkerhed. 
+Paul Harrison: klarinet/sopransax/altsax. Niklas Carlsson: trombone/voc. Sean Moyses: banjo/voc. Søren Stausbøl: piano/voc. Peter Williams: kontrabas. Mikkel Find: trommer.</p>
             </div>
         </div>
         <!-- /.row -->
@@ -58,7 +61,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Pete Allen</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+                <p>Den engelske klarinettist Pete Allen har været en markant solist hos bl. a. Kenny Ball og Terry Lightfoot. Han er født i 1954, og er således stadig en musiker i sin bedste alder. Allen er en ægte repræsentant for britisk revival-jazz, når den er allerbedst. Han ledsages på danmarksturnéen af det rutinerede Fionia Jazzband med hvem han i 2013 under eget navn indspillede ”A Night in Nyborg” i 2013. 
+Pete Allen: klarinet. Erik Sørensen: sax. Tom Nissen: banjo. Jens Holgersen: kontrabas. Jazper Lindenhoff: trommer.</p>
             </div>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
@@ -67,7 +71,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">River Jazz</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 				varius vitae.</p>
+                <p>2015 blev året, hvor musikerne i River Band kunne sætte flueben ud for endnu en målsætning. I april optrådte de på Preservation Hall i New Orleans. River Band har sin helt egen lyd: en velrystet cocktail af det bedste fra trad, swing, blues og latin, serveret med jydsk lune. 
+Torben Lassen: trompet, mundharpe, vokal. Karen Sørensen: klaver. Allan Bjerregaard: trombone, vokal. Filt Kristensen: tenorsax, klarinet. Jørgen Nielsen: bas. Nikolai Seidelin Banke: trommer.</p>
             </div>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
@@ -76,7 +81,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Miriam </a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+                <p>Den afrikanske jazzdronning vender tilbage til klubben. Denne gang med et orkester, hvor bunden lægges af Hammondorganisten Kjeld Lauritsen og hans yderst kompetente band. Miriam mestrer både jazz, blues, gospel og soul, og denne besætning er klar til at følge med hele vejen. 
+Miriam Mandipira: vokal. Jens Klüver: sax. Uffe Steen: guitar. Kjeld Lauritsen: orgel. Esben Bach: trommer.</p>
             </div>
         </div>
         <!-- /.row -->
@@ -89,7 +95,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Vestre Jazzværk</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+                <p>et af de bedste navne inden for dansk traditionel jazz. Den sydvestjyske konstellation har de senere år fået et fynsk islæt, og Vestre Jazzværk er stadig et rigtig godt bud på en lørdag eftermiddag med god, professionel jazz, direkte fra musikkens kilder. 
+Aage Jespersen: trompet. Poul Schmidt basun, vokal. Ole Weibel klarinet, vokal. Tom Nissen: banjo, vokal. Poul Erik Philipsen: bas. Hans Rømer: trommer.</p>
             </div>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
@@ -98,7 +105,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">Max Lagers</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 				varius vitae.</p>
+                <p>Med udgangspunkt i Göteborgs frodige jazzliv har Max Lagers siden 1979 dyrket New Orleans revival jazzen. I det øvrige norden kalder man det ”Gladjazz”, ukompliceret musik, som man bliver glad i låget af. Max Lager himself skal du kigge langt efter: han findes kun som figur i en roman af orkestrets pianist! 
+Bosse Ericsson: trompet, cornet. Anders Ekström: klarinet, sax. Roland Ödborg: trombone. Anders Rylander: banjo. Ulf Albrektsson: piano. Ored Borgsund: bas. Lennart Carlsson: trommer.</p>
             </div>
             <div class="col-md-4 portfolio-item">
                 <a href="#">
@@ -107,7 +115,8 @@ include "header.php";?>
                 <h3>
                     <a href="#">All Time Jazzband</a>
                 </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+                <p>All Time Jazzband leverer jazz til tiden, og til næsten enhver lejlighed. De fem fra Silkeborg har markeret sig med en utraditionel tilgang til traditionen. All Time Jazz Band har hele tre forsangere, og instrumentalisterne behersker flere instrumenter, så det er et band med et meget varieret udtryk. 
+Ole Rosendal: trompet, flygelhorn, vokal. Erik Schreiber: tenorsax, klarinet. Jan G. Larsen: banjo, vokal. Morten Tværmose Andersen: kontrabas, tuba, vokal. Jørgen ”Mulle” Mulbjerg: trommer, vokal.</p>
             </div>
         </div>
         <!-- /.row -->
