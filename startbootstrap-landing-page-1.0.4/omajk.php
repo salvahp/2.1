4a3b1f16b853afc5f09303f1bf93b75e672e3b4b
salvador pavon <salvahp@gmail.com>
2015-09-30 20:36:56 +0200

    ar
    tw

--- startbootstrap-landing-page-1.0.4/omajk.php
+++ startbootstrap-landing-page-1.0.4/omajk.php
@@ -44,7 +44,7 @@ include "header.php";?>
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">3D Device Mockups<br>by PSDCovers</h2>
+                    <h2 class="section-heading">Bestyrrelsen</h2>
                     <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
@@ -66,7 +66,7 @@ include "header.php";?>
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Bliv medlem<br>det er sjooooovt</h2>
+                    <h2 class="section-heading">Kontakt</h2>
                     <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
