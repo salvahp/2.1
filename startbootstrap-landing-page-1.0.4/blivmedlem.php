a182b5868c7d3398df1c4131eb09bbfc71ef6295
Dan Mortensen <enklopida@gmail.com>
2015-09-30 14:21:32 +0200

    Kontakt formular
    Virker ikke, men er til stede, visuelt

--- startbootstrap-landing-page-1.0.4/blivmedlem.php
+++ startbootstrap-landing-page-1.0.4/blivmedlem.php
@@ -1,4 +1,3 @@
-
 		
 <?php 
 $title ="Aarhus Jazzklub - Bliv medlem";
@@ -23,8 +22,16 @@ include "header.php";?>
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Death to the Stock Photo:<br>Special Thanks</h2>
-                    <p class="lead">A special thanks to <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a> for providing the photographs that you see in this template. Visit their website to become a member.</p>
+                    <h2 class="section-heading">Bliv medlem</h2>
+                    <p class="lead">Som medlem af Aarhus Jazzklub opnår du en række kontante fordele:<br>
+<ul class="list-group">
+	<li>Kontingent for et år er kr. 300,-</li>
+	<li>Vi afholder vinterjazz fra sidst i september til maj, ca. 25 arrangementer</li>
+	<li>Vi afholder 8 arrangementer i Aarhus Jazz Festival</li>
+	<li>Du modtager vores jazzavis to gange årligt i postkassen</li>
+	<li>Medlemsfordele: Du kommer ind i Randers-Silkeborg-Grenå-Horsens og Jazz selskabet, Århus til medlemspris.</li>
+</ul>
+</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                     <img class="img-responsive" src="img/ipad.png" alt="">
@@ -45,8 +52,18 @@ include "header.php";?>
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">3D Device Mockups<br>by PSDCovers</h2>
-                    <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+                    <h2 class="section-heading">Tilmelding</h2>
+					<p>
+					<ul class="list-group">
+						<li>Udfyld dette skema og du er medlem af Aarhus Jazzklub. Du opnår en række fordele som medlem.</li>
+						<li>Prisen for medlemskab er kr. 300,- pr. år.</li>
+						<li>Du modtager et girokort med posten, men du kan også melde dig ind i døren ved vores arrangementer "Hos Anders".</li>
+						<li>Kvitteringstalonen er dit medlemsbevis.
+					Medlemskort afhentes i døren.</li>
+						<li>Husk at skrive e-mail så kan kan informere om vigtige ting.</li>
+						<li>Se også vores optagelsesbetingelser i vedtægterne.</li>
+					</ul>
+					</p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
                     <img class="img-responsive" src="img/dog.png" alt="">
@@ -67,11 +84,83 @@ include "header.php";?>
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Bliv medlem<br>det er sjooooovt</h2>
-                    <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
-                </div>
-                <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img class="img-responsive" src="img/phones.png" alt="">
+                    <!-- kontakt form -->
+						<div style="overflow:hidden;width:490px;" class="pageContentSection cmsContentStandardContact_formWrap">
+								  	
+								  	
+<script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script><link type="text/css" rel="stylesheet" href="http://www.aarhusjazzklub.dk/admin/Systemfiles/Jquery/jqueryui/jquery-ui-1.10.3/css/smoothness/jquery-ui-1.10.3.custom.min.css"><script type="text/javascript" src="http://www.aarhusjazzklub.dk/admin/Systemfiles/Jquery/i18n/ui.datepicker-da.js"></script><style type="text/css">#contactform_34657742375_form .contactform-section {margin-bottom:10px;}#contactform_34657742375_form .contactform-row{clear:both;margin-bottom:5px;}#contactform_34657742375_form .contactform-row.ui-field-contain{border-bottom:0;}#contactform_34657742375_form .contactform-row label{display:inline;}#contactform_34657742375_form #contactform_34657742375_error {display:none;color:red;}#contactform_34657742375_form .contactform-row label.radiocheckbox-label{display:inline;margin-left:.5em;}#contactform_34657742375_form .contactform-row img{display:inline;margin-left:.5em;}#contactform_34657742375_form .contactform-row input[type=text],.contactform-row textarea,.contactform-row select{min-width:90%;}#contactform_34657742375_form .contactform-row input.captcha_field{min-width:50px;max-width:123px;}#contactform_34657742375_form .contactform-row input:not([type='checkbox']):not([type='radio']),.contactform-row textarea{padding:5px;font-size:13px;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;}#contactform_34657742375_form .contactform-row input:not([type='checkbox']):not([type='radio']),.contactform-row textarea{border:1px solid #cccccc !important;}.ui-datepicker{ z-index: 9999999 !important;}#contactform_34657742375_form .contactform-row textarea{height:5em !important;}#contactform_34657742375_form .contactform-row img.captcha_img{margin:1em 0;display:block;}#contactform_34657742375_form .contactform-submit-btn{float:left;}#contactform_34657742375_form .contactform-submit-btn{
+						padding: 8px 18px;
+						background: #ffffff;
+						border: 1px solid #999999;
+						font-weight: bold;
+						-webkit-border-radius: 4px;
+						-moz-border-radius: 4px;
+						border-radius: 4px;}</style><script type="text/javascript">function contactform_34657742375_validate_checkbox_function() {
+						var checkboxGroupName = '';
+						var checkboxGroupID = '';
+						var numCheckboxes = 0;
+						var numCheckboxesChecked = 0;
+						var numInvalid = 0;
+						$('.contactform_34657742375_validate_checkbox').each(function () {
+														
+							checkboxGroupName = this.name.substring(0, this.name.lastIndexOf('['))
+							checkboxGroupID = this.id.substring(0, this.id.lastIndexOf('_'));
+							numCheckboxes = $('input[id^='+checkboxGroupID+']').length;
+							numCheckboxesChecked = $('input[id^='+checkboxGroupID+']:checked').length;
+							
+							if (numCheckboxesChecked < 1)
+							{
+								$('.'+checkboxGroupID+'_img').attr('src', 'http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/no.gif');
+								numInvalid++;
+							}
+							else
+							{
+								$('.'+checkboxGroupID+'_img').attr('src', 'http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/yes.gif'); 								
+							}
+						});
+						return numInvalid;
+					   }</script><a name="contactform_34657742375_form_anchor" data-role="none"></a><form id="contactform_34657742375_form" method="post" onsubmit="return contactform_34657742375_validate_on_submit();" action="/bliv-medlem#contactform_34657742375_form_anchor"><input type="hidden" name="root" value="http://www.aarhusjazzklub.dk/admin"><input type="hidden" name="validate_message" value="Et eller flere felter mangler at blive udfyldt..."><input type="hidden" name="confirm_save" value="Vil du gemme?"><input type="hidden" name="confirm_delete" value="Er du sikker på, at du vil slette?"><div class="contactform-section"><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_name">Navn</label><img id="contactform_34657742375[name]_img" src="http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/no.gif"></div>
+							<input value="" type="text" name="contactform_34657742375[name]" id="contactform_34657742375_name" class="validate_nameStrict" onkeyup="validate_on_change('','','contactform_34657742375_form');">
+							</div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_street">Gade og nr.</label><img id="contactform_34657742375[street]_img" src="http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/no.gif"></div>
+							<input value="" type="text" name="contactform_34657742375[street]" id="contactform_34657742375_street" class="validate_address" onkeyup="validate_on_change('','','contactform_34657742375_form');">
+							</div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_city">Postnr. og by</label><img id="contactform_34657742375[city]_img" src="http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/no.gif" data-pin-nopin="true"></div>
+							<input value="" type="text" name="contactform_34657742375[city]" id="contactform_34657742375_city" class="validate_postal_city" onkeyup="validate_on_change('','','contactform_34657742375_form');">
+							</div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_email">E-mail</label><img id="contactform_34657742375[email]_img" src="http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/no.gif"></div>
+							<input value="" type="text" name="contactform_34657742375[email]" id="contactform_34657742375_email" class="validate_email" onkeyup="validate_on_change('','','contactform_34657742375_form');">
+							</div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_phone">Telefon</label><img id="contactform_34657742375[phone]_img" src="http://www.aarhusjazzklub.dk/admin/Mediafiles/Images/no.gif" data-pin-nopin="true"></div>
+							<input value="" type="text" name="contactform_34657742375[phone]" id="contactform_34657742375_phone" class="validate_phone" onkeyup="validate_on_change('','','contactform_34657742375_form');">
+							</div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_subject">Emne</label>&nbsp;</div><input type="hidden" name="contactform_34657742375[subject]" value="Ja, jeg vil gerne være medlem af Århus Jazzklub"><div id="contactform_34657742375_subject">Ja, jeg vil gerne være medlem af Århus Jazzklub</div> </div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_message">Besked</label>&nbsp;</div>
+							<textarea name="contactform_34657742375[message]" id="contactform_34657742375_message"></textarea>
+							</div><div class="contactform-row ui-field-contain">
+							<div><label for="contactform_34657742375_verification">For at forhindre spam bedes du indtaste tegnene i feltet herunder:</label></div>
+							<div><img class="captcha_img" src="http://www.aarhusjazzklub.dk/admin/Systemfiles/captcha.php?c=bw78QAkhtwbh6e4" alt=""></div>
+							<input name="contactform_34657742375[verification]" id="contactform_34657742375_verification" class="validate_text captcha_field" maxlength="6" type="text">
+							<input name="contactform_34657742375[captcha_enc]" type="hidden" value="bw78QAkhtwbh6e4">
+							</div><div class="contactform-row">
+						<div id="contactform_34657742375_error"></div>
+					   </div><div class="contactform-row">
+						<button class="contactform-submit-btn" type="submit">Send</button>
+						<div style="clear:both;"></div>
+					   </div></div><script type="text/javascript"> function contactform_34657742375_validate_on_submit() {
+								var numInvalidCheckbox = contactform_34657742375_validate_checkbox_function();
+								if (numInvalidCheckbox > 0) {
+									alert($('input[name=validate_message]').val()); return false;
+								} else {
+									return validate_on_submit(document.getElementById('contactform_34657742375_form'));
+								}
+							} $(document).ready(function () {
+								validate_on_change('','','contactform_34657742375_form');
+								contactform_34657742375_validate_checkbox_function();
+							}); </script></form>
+							</div>		
+					<!-- /kontakt form -->
                 </div>
             </div>
 
