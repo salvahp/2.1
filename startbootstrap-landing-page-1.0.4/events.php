c38262c47357e9f3db7dfe87ea69b70da490c04a
salvador pavon <salvahp@gmail.com>
2015-09-30 14:19:38 +0200

    events
    side

--- startbootstrap-landing-page-1.0.4/events.php
+++ startbootstrap-landing-page-1.0.4/events.php
@@ -8,8 +8,109 @@ include "header.php";?>
 
     <!-- Page Content -->
 
-
-	
+<!-- Page Header -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Events 
+                    <small>Her kan man se events osv.. </small>
+                </h1>
+            </div>
+        </div>
+   <!-- /.row -->
+   
+   <!-- Projects Row -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/martinis_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Martinis </a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/madstolling_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Mads Tolling</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 				varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/paulharrison_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Paul Harrison</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+	 <!-- Projects Row -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/peteallen_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Pete Allen</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/riverjazz_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">River Jazz</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 				varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/mandipira_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Miriam </a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+         <!-- Projects Row -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/vestrejazz_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Vestre Jazzværk</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/maxlagers_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">Max Lagers</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 				varius vitae.</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="img/alltimejazzband_700x400.png" alt="">
+                </a>
+                <h3>
+                    <a href="#">All Time Jazzband</a>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna 					varius vitae.</p>
+            </div>
+        </div>
+        <!-- /.row -->
 
 
     <!-- Footer + banner-->
